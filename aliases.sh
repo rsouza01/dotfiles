81f0faa9b80d6177a82c5dae4c630388f72448a8
Rodrigo Alvares de Souza <rodrigo.alvaresdesouza@jumbo.com>
2021-07-13 15:15:06 +0200

    Aliases file added

---
+++ aliases.sh
@@ -0,0 +1,17 @@
+# ALIASES AND USEFUL STUFF
+
+alias mci="mvn clean install"
+alias mcit="mvn clean test"
+
+
+alias gcm="git checkout master"
+alias gfpm="git fetch && git pull origin master"
+alias gits="git status"
+alias gitd="git diff"
+alias glp="git log --pretty=oneline"
+
+alias tailf="tail -n 100 -f"
+
+
+
+
